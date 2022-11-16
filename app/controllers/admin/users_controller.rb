21b18d62f364fa2d46af825b212188d24cd2d4bd
SakiOjima <100123445+Jiima33@users.noreply.github.com>
2022-11-16 07:27:24 +0000

    管理者側ユーザーの登録順追加

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -2,7 +2,8 @@ class Admin::UsersController < ApplicationController
   before_action :authenticate_admin!
   
   def index
-    @users = User.page(params[:page])
+    @q = User.ransack(params[:q])
+    @users = @q.result.page(params[:page])
   end
   
   def show 
@@ -23,9 +24,18 @@ class Admin::UsersController < ApplicationController
     end
   end
   
+  def search
+    @q = User.search(search_params)
+    @users = @q.result
+  end 
+  
   private
     def user_params
       params.require(:user).permit(:name, :introduction, :profile_image, :is_stopped)
+    end
+    
+    def search_params
+      params.require(:q).permit()
     end
 end
 
